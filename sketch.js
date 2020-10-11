784c5f61a72f996ad39e35dac73d298986eb1474
gautami03 <66514498+gautami03@users.noreply.github.com>
2020-10-11 18:40:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var ball;
+var database,position;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPosition=database.ref("Ball/Position");
+    ballPosition.on("value",readposition,showerror);
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(10,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-10);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+10);
+    }
+}
+    drawSprites();
+}
+
+function changePosition(x,y){
+database.ref("Ball/Position").set({
+ "x":position.x+x,
+ "y":position.y+y
+})
+}
+function readposition(data){
+position=data.val();
+ball.x=position.x;
+ball.y=position.y;
+}
+
+function showerror(){
+console.log("error");
+}
